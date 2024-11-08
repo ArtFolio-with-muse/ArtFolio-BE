ea8daf5890c61848cc6942725922bfde036c4de2
minseob <minseob15@naver.com>
2024-11-08 15:16:32 +0900

    테스트겸 컨트롤러 작성

---
+++ artfolio-core/src/main/java/kr/art_folio/artfolio_core/domain/exhibition/controller/ExhibitionController.java
@@ -0,0 +1,11 @@
+package kr.art_folio.artfolio_core.domain.exhibition.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/exhibition")
+@RequiredArgsConstructor
+public class ExhibitionController {
+}
